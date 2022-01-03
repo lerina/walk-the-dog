c46190e770740887d1ffd851333b4c4076284da3
Lerina J-Y Razafy <lerina.razafy@gmail.com>
2022-01-02 21:40:06 -0500

    blackHat Rust

---
+++ Rust/sha1_cracker/src/bin/02.rs
@@ -0,0 +1,28 @@
+use std::{env, error::Error, fs};
+
+const SHA1_HEX_STRING_LENGTH: usize = 40;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let cmd_args: Vec<String> = env::args().collect();
+
+    if cmd_args.len() != 3 {
+        println!(
+            "Usage: 
+     sha1_craker: <wordlist.txt> <sha1_hash>"
+        );
+        return Ok(());
+    }
+
+    let hash_to_crack = cmd_args[2].trim();
+    if hash_to_crack.len() != SHA1_HEX_STRING_LENGTH {
+        return Err("sha1 hash is not valid".into());
+    }
+
+    let wordlist_file = fs::read_to_string(&cmd_args[1])?;
+
+    for line in wordlist_file.split('\n') {
+        println!("{}", line.trim());
+    }
+
+    Ok(())
+}
