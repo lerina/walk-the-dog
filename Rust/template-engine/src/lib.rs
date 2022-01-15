189f90117b1c7f4f258fb42c3cce69786fb1d2b5
Lerina J-Y Razafy <lerina.razafy@gmail.com>
2022-01-15 15:55:15 -0500

    TDD rust

--- Rust/template-engine/src/lib.rs
+++ Rust/template-engine/src/lib.rs
@@ -37,12 +37,54 @@ pub struct ExpressionData {
     pub tail: Option<String>,
 }
 
+/// Checking if the two matching tags are contained within the input string
+pub fn check_matching_pair(input: &str, symbol1: &str, symbol2: &str) -> bool {
+    input.contains(symbol1) && input.contains(symbol2)
+}
+
+///  Checks if a symbol string, for example, '{%', is contained within another string.
+pub fn check_symbol_string(input: &str, symbol: &str) -> bool {
+    input.contains(symbol)
+}
+
+
+/// Entry point for parser. Accepts an input statement 
+/// and tokenizes it into one of an if tag, a for tag, or a template variable.
+pub fn get_content_type(input_line: &str) -> ContentType {
+    let is_tag_expression = check_matching_pair(&input_line, "{%", "%}");
+    let is_for_tag = (  check_symbol_string(&input_line, "for") && 
+                        check_symbol_string(&input_line, "in")
+                     ) 
+                     || check_symbol_string(&input_line, "endfor") ;
+    //NOTE: tmp_sub
+    ContentType::Unrecognized
+}
+
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn check_symbol_string_test() {
+        assert_eq!(true, check_symbol_string("{{Hello}}", "{{"));
+    }
+
+    #[test]
+    fn check_matching_pair_test() {
+        assert_eq!(true, check_matching_pair("{{Hello}}", "{{", "}}"));
+    }
+
+    #[test]
+    fn check_template_var_test() {
+        let content = ExpressionData{
+            head: Some("Hi ".to_string()),
+            variable: "name".to_string(),
+            tail: Some(" , welcome".to_string()),
+        };
+
+        assert_eq!(ContentType::TemplateVariable(content),
+                   get_content_type("Hi {{name}} , welcome")
+                   );
     }
 }
