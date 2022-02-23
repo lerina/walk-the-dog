c695b7c40b975acfa69e706b6b6a1f4a3bc8ca11
Lerina J-Y Razafy <lerina.razafy@gmail.com>
2022-02-22 21:18:30 -0500

    port to rust

---
+++ Rust/Hangperson/h01.py
@@ -0,0 +1,19 @@
+import random
+#Step 1 
+
+word_list = ["aardvark", "baboon", "camel"]
+
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+secret_word = random.choice(word_list)
+
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+user_guess = (input("Guess a letter: ")).lower()[0]
+
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+for letter in secret_word:
+    if letter == user_guess:
+        print("Right")
+    else:
+        print("Wrong")
+
+
