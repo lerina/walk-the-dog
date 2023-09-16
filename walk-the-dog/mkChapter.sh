4e7cf45f2d264878b8833107fcd056e43862a09b
Lerina J-Y Razafy <lerina.razafy@gmail.com>
2023-09-16 13:57:10 -0400

    tagging the chapters

--- walk-the-dog/mkChapter.sh
+++ walk-the-dog/mkChapter.sh
@@ -1,5 +1,11 @@
+#!/bin/sh
+
+# USAGE: ./mkChapter chap<number>
+
 TARGET=$1
 mkdir code-along/$TARGET
 cp -a src/ www/ README.md Cargo.toml run.sh code-along/$TARGET
 tree code-along/$TARGET
+git tag $TARGET
+git push origin $TARGET
 
