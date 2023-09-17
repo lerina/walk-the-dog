5e12bb60575a64744e3b63b5bc161e1b68c53acb
Lerina J-Y Razafy <lerina.razafy@gmail.com>
2023-09-17 19:55:55 -0400

    Drawing a triangle

--- walk-the-dog/src/lib.rs
+++ walk-the-dog/src/lib.rs
@@ -1,11 +1,36 @@
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 use web_sys::console;
 
+
 #[wasm_bindgen(start)]
-pub fn main_js() -> Result<(), JsValue> {
+pub fn main_js() { // -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
 
-    console::log_1(&JsValue::from_str("Hello world!"));
+//    console::log_1(&JsValue::from_str("Hello world!"));
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let canvas = document
+                    .get_element_by_id("canvas")
+                    .unwrap()
+                    .dyn_into::<web_sys::HtmlCanvasElement>()
+                    .unwrap();
 
-    Ok(())
+    let context = canvas
+                    .get_context("2d")
+                    .unwrap()
+                    .unwrap()
+                    .dyn_into::<web_sys::CanvasRenderingContext2d>()
+                    .unwrap();
+
+        context.move_to(300.0, 0.0); // top of triangle
+        context.begin_path();
+        context.line_to(0.0, 600.0); // bottom left of triangle
+        context.line_to(600.0, 600.0); // bottom right of triangle
+        context.line_to(300.0, 0.0); // back to top of triangle
+        context.close_path();
+        context.stroke();
+        context.fill();
+
+ //   Ok(())
 }
